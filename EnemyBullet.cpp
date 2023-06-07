3463c8f6e03b0f6b13f4a5f44c21b64fba30519b
TonosakiRen <ren221603@gmail.com>
2023-06-07 16:15:46 +0900

    AL02_09_ex1

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -13,7 +13,22 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 
 	worldTransform_.translation_ = position;
 
+	//Z方向に延びた形状
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+
+	//Y軸周り角度(θy)
+	worldTransform_.rotation_.y = std::atan2(velocity_.z, velocity_.x);
+	
+	Matrix4x4 tmp = MakeRotateYMatrix(-std::atan2(velocity_.z, velocity_.x));
+	Vector3 velocityZ = Transform(velocity_, tmp);
+
+	worldTransform_.rotation_.x = std::atan2(-velocityZ.y, velocityZ.z);
+
 	velocity_ = velocity;
+
+	worldTransform_.UpdateMatrix();
 }
 void EnemyBullet::Update() {
 
